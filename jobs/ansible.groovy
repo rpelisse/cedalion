1d783771f6e71bb328c40cc02b4910421e137995
Romain Pelisse <belaran@gmail.com>
2022-01-10 17:38:23 +0100

    Fixup

--- jobs/ansible.groovy
+++ jobs/ansible.groovy
@@ -1,5 +1,5 @@
 new ansible.Builder(collectionName:'redhat-csp-download').build(this)
 new ansible.Builder(collectionName:'jws-ansible-playbook').build(this)
 new ansible.Builder(collectionName:'ansible_collections_jcliff').build(this)
-new ansible-ci.Builder(projectName:'molecule-delegate', git_url: 'https://github.com/rpelisse/').build(this)
+new ansibleCi.Builder(projectName:'molecule-delegate', git_url: 'https://github.com/rpelisse/').build(this)
 EapView.jobList(this, 'Ansible Collections', 'ansible-collection-*')
